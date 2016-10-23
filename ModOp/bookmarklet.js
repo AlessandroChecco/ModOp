8c830a629341d447c3ab39f4cb281eacf5c0905b
Alessandro Checco <checcoa@tcd.ie>
2016-10-23 04:29:04 +0100

    Added Chrome extension

---
+++ ModOp/bookmarklet.js
@@ -0,0 +1,1 @@
+ (function () { var jsCode=document.createElement('script');jsCode.setAttribute('src', 'https://dl.dropboxusercontent.com/u/13815598/ModOp.js');document.body.appendChild(jsCode);}());
